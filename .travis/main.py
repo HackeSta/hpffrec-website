db1c955cc70cfb0a7b694dccd72051e2cff64f7d
Haider Ali Punjabi <haiderali@cyberservices.com>
2019-07-09 12:19:28 +0530

    Trying Travis

---
+++ .travis/main.py
@@ -0,0 +1,9 @@
+import json
+import os
+import requests
+files = ['latest', 'stories_top100_thisweek', 'stories_top100_thismonth', 'stories_top100_thisyear', 'stories_top100_alltime', 'authors_top100_thisweek', 'authors_top100_thismonth', 'authors_top100_thisyear', 'authors_top100_alltime', 'stats']
+PATH = os.environ['DATA_SOURCE']
+for file in files:
+    r = requests.get(PATH + file + ".json")
+    data = json.loads(r.text)
+    json.dump(data,open("/data/"+file+".json", "w"))
