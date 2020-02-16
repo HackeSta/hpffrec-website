358726a97d6fa51494e17fabac142324f1f36668
= <haiderali@cyberservices.com>
2020-02-16 16:25:38 +0000

    Test 1

--- .travis/main.py
+++ .travis/main.py
@@ -3,6 +3,8 @@ import os
 import requests
 files = ['latest', 'stories_top100_thisweek', 'stories_top100_thismonth', 'stories_top100_thisyear', 'stories_top100_alltime', 'authors_top100_thisweek', 'authors_top100_thismonth', 'authors_top100_thisyear', 'authors_top100_alltime', 'stats', "genre_filters","character_filters"]
 PATH = os.environ['DATA_SOURCE']
+if not os.path.exists('data'):
+    os.makedirs('data')
 for file in files:
     r = requests.get(PATH + file + ".json")
     data = json.loads(r.text)
